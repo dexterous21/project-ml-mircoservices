69ec586c19e427afd4b003a71e5b77ab3b76f27a
EC2 Default User <ec2-user@ip-172-31-64-106.ec2.internal>
2023-03-07 01:03:38 +0000

    project testing

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="dexterous21/idris-app"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag idris-app $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
